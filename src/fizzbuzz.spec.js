fdbbae9812afacec8e2a0b532c42c904822a709d
michal.lipski <michal.lipski@newvoicemedia.com>
2018-06-25 13:29:33 +0200

    added node-modules

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,4 +4,8 @@ describe('Fizz Buzz', function () {
   it('has tests', function () {
     expect(fizzbuzz).toBeDefined();
   });
+
+  it('has tests', function () {
+    expect(fizzbuzz()).toEqual("")
+  });
 });
