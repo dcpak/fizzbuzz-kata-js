1badf14180dc6bbe640ef4d745c32b98ff9303d2
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2020-06-23 14:53:28 +0200

    Implemented digit in number.

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,8 @@
 module.exports = function fizzbuzz(n) {
   let r = '';
-  if(n%3 === 0)
+  if(n%3 === 0 || ('' + n).indexOf('3') !== -1)
     r = 'Fizz';
-  if(n%5 === 0)
+  if(n%5 === 0 || ('' + n).indexOf('5') !== -1)
     r += 'Buzz';
   if(!r)
     r = n;
