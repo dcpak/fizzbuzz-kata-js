3e7e14625acf5152b4686ed680ccc17d32e05cf3
Przemysław Kasprzak <przemyslaw.kasprzak@orange.com>
2020-06-23 15:05:09 +0200

    Implemented new condition for digit 7.

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -4,6 +4,8 @@ module.exports = function fizzbuzz(n) {
     r = 'Fizz';
   if(n%5 === 0 || ('' + n).indexOf('5') !== -1)
     r += 'Buzz';
+  if(n%7 === 0 || ('' + n).indexOf('7') !== -1)
+    r += 'Bar';
   if(!r)
     r = n;
   return r;
